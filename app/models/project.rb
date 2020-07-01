3830d32bd70f2c275ac1fba524d05d6e1abcc104
jameszhou123 <jameszhou@outlook.com>
2020-07-01 02:10:33 +0100

    add milestone scaffold, implement project/milestone association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :milestones, :dependent => :delete_all
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :milestones
   resources :projects
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   get 'about', to: 'pages#about'
