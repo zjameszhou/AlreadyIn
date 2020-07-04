2c531c552af1e3280cd0a307f0d0f8aad6c1b0e6
jameszhou123 <jameszhou@outlook.com>
2020-07-05 00:21:54 +0100

    updated project controller to add new record to ProjectUser table when creating new project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  has_many :project_users
+  has_many :project_users, :dependent => :delete_all
   has_many :users, through: :project_users
   has_many :milestones, :dependent => :delete_all
 end
