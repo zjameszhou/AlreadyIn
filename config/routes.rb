b40280c0cc5cb9fea8f49189afcca52657a2dddc
jameszhou123 <jameszhou@outlook.com>
2020-07-04 16:58:28 +0100

    sendgrid config

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   resources :milestones
   resources :projects
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  devise_for :users, controllers: { confirmations: 'confirmations' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }, controllers: { confirmations: 'confirmations' }
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   root to: 'pages#home'
