437b3ef7c2f69a7c54a6731883db280b51ba0a05
jameszhou123 <jameszhou@outlook.com>
2020-07-05 18:47:05 +0100

    updated profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  
   resources :milestones
   resources :projects
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  get 'profile', to: 'users#show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   root to: 'pages#home'
