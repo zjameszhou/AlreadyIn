4a65e82e34ba0c3acaf001ae0fa5f61859c9858b
jameszhou123 <jameszhou@outlook.com>
2020-08-04 23:43:39 +0100

    added comment to milestones

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,7 +15,11 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+//= require cable
 //= require turbolinks
 //= require materialize
 //= require materialize-sprockets
+//= require commontator/application
--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,3 +1,4 @@
 class Milestone < ApplicationRecord
   belongs_to :project
+  acts_as_commontable
 end
