270070f526c9a4a5058c5b111e321635e6f28019
jameszhou123 <jameszhou@outlook.com>
2020-08-08 18:29:00 +0100

    page style update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_204504) do
+ActiveRecord::Schema.define(version: 2020_08_08_164848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_204504) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "progress"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
